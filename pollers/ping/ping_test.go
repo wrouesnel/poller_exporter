7d1628263458046553df293ce064cc3faade78c1
William Rouesnel <William.Rouesnel@netregistry.com.au>
2015-11-23 15:44:09 +1100

    Opt out the sudo tests

--- pollers/ping/ping_test.go
+++ pollers/ping/ping_test.go
@@ -6,6 +6,8 @@ import (
 	"time"
 	"net"
 	"flag"
+	"os/user"
+	"fmt"
 )
 
 func init() {
@@ -14,10 +16,16 @@ func init() {
 
 func TestPing(t *testing.T) {
 	assert := assert.New(t)
+    
+    user, err := user.Current()
+    assert.Nil(err)
+    if user.Uid == "0" {
+	    ok, _ := Ping(net.ParseIP("127.0.0.1"), time.Second)
+	    assert.True(ok, "Localhost IPv4 Ping")
 
-	ok, _ := Ping(net.ParseIP("127.0.0.1"), time.Second)
-	assert.True(ok, "Localhost IPv4 Ping")
-
-	ok6, _ := Ping(net.ParseIP("::1"), time.Second)
-	assert.True(ok6, "Localhost IPv6 Ping")
+	    ok6, _ := Ping(net.ParseIP("::1"), time.Second)
+	    assert.True(ok6, "Localhost IPv6 Ping")
+	} else {
+	    fmt.Println("Skipping ping test since not superuser")
+	}
 }
